337529d31299c5ab9acde16d39a8a130143c4a36
Sofian <74557858+Bsofian1@users.noreply.github.com>
2021-05-21 13:35:51 +0200

    before adding date

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
+import React from "react";
 import "./App.css";
 import user from "./data/usersData";
 import Header from "./component/Header";
-import Add from "./component/Add"
+import Add from "./component/Add";
+import Main from "./component/Main";
 
-function App() {
-  const currentUser = user[0];
+let currentUser = user[0];
+let expenses = [];
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expenseName: "",
+      totalAmount: null,
+      date: null,
+      ownerExpense: "",
+      userExpense: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <div className="App">
-      <Header user={currentUser} />
-      <Add />
-    </div>
-  );
+  //work for all text base input as soon they have a name props than match the state
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+  handleMultiChange(event) {
+    this.setState(() => {
+      return {
+        userExpense: event.target.value,
+      };
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    expenses.push(this.state);
+    this.setState({
+      expenseName: "",
+      totalAmount: null,
+      date: null,
+      ownerExpense: "",
+      userExpense: [],
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header user={currentUser} />
+        <Main data={expenses} />
+        <Add
+          handleSubmit={this.handleSubmit}
+          onChange={(value) => this.handleChange(value)}
+          onMultiChange={(value) => this.handleMultiChange(value)}
+          {...this.state}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/Add.js
+++ src/component/Add.js
@@ -3,8 +3,18 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import users from "../data/usersData";
 
-const Add = (_) => {
+const names = users.map((user) => user.name);
+
+const Add = (props) => {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -24,7 +34,65 @@ const Add = (_) => {
         aria-labelledby="form-dialog-title"
       >
         <DialogTitle id="form-dialog-title">Add a new expense</DialogTitle>
-        <DialogContent></DialogContent>
+        <DialogContent>
+          <form onSubmit={props.handleSubmit}>
+            <TextField
+              type="text"
+              id="standard-basic"
+              label="Expense Name"
+              name="expenseName"
+              value={props.expenseName}
+              onChange={(event) => props.onChange(event)}
+            />
+            <br />
+            <TextField
+              type="number"
+              id="standard-basic2"
+              label="Amount"
+              name="totalAmount"
+              value={props.totalAmount}
+              onChange={(event) => props.onChange(event)}
+            />
+            <br />
+            <InputLabel id="demo-mutiple-checkbox-label">Paid by</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              fullWidth
+              id="demo-simple-select"
+              name="ownerExpense"
+              value={props.ownerExpense}
+              onChange={(event) => props.onChange(event)}
+            >
+              {names.map((name) => (
+                <MenuItem value={name}>{name}</MenuItem>
+              ))}
+            </Select>
+            <br />
+            <InputLabel id="demo-mutiple-checkbox-label1">
+              Shared with
+            </InputLabel>
+            <Select
+              fullWidth
+              labelId="demo-mutiple-checkbox-label"
+              id="demo-mutiple-checkbox"
+              multiple
+              value={props.userExpense}
+              onChange={(event) => props.onMultiChange(event)}
+              input={<Input />}
+              renderValue={(selected) => selected.join(", ")}
+            >
+              {names.map((name) => (
+                <MenuItem key={name} value={name}>
+                  <Checkbox checked={props.userExpense.indexOf(name) > -1} />
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
+            </Select>
+            <br />
+
+            <button>Submit</button>
+          </form>
+        </DialogContent>
         <DialogActions></DialogActions>
       </Dialog>
     </div>
